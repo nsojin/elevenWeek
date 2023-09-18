83730617219a9c186df7efffd8607735a374e6bd
nojinee <dsg03-2@daun.met>
2023-09-19 01:08:17 +0900

    toolbar

--- app/src/main/java/com/example/elevenweek/MainActivity.kt
+++ app/src/main/java/com/example/elevenweek/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.elevenweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.elevenweek.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search, menu)
+        return true
     }
 }
